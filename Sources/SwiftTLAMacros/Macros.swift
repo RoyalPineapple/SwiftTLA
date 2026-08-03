6d9c39bf1c51637261a76c4acceec8e99f150a67
Alex Odawa <alex@squareup.com>
2026-08-04 01:46:29 +0200

    #tla → struct TLA. TLA+ branding throughout

--- Sources/SwiftTLAMacros/Macros.swift
+++ Sources/SwiftTLAMacros/Macros.swift
@@ -1,5 +1,5 @@
-@freestanding(declaration, names: named(Spec))
-public macro spec(_ body: () -> Void) = #externalMacro(module: "SwiftTLAPlugin", type: "ModelMacro")
+@freestanding(declaration, names: named(TLA))
+public macro tla(_ body: () -> Void) = #externalMacro(module: "SwiftTLAPlugin", type: "ModelMacro")
 
 @attached(member, names: named(Action), named(initial))
 public macro TLASpec() = #externalMacro(module: "SwiftTLAPlugin", type: "TLASpecAttachedMacro")
