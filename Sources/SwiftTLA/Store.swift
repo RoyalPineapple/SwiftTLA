8f3aa4a69dee00a3eecaffe4be2c916e8e88567c
Alex Odawa <alex@squareup.com>
2026-08-04 13:28:15 +0200

    Store: machine IS the state, no duplicate snapshot

--- Sources/SwiftTLA/Store.swift
+++ Sources/SwiftTLA/Store.swift
@@ -3,16 +3,14 @@ import Observation
 @MainActor
 @Observable
 public final class Store<M: TLAMachine> {
-    private var machine: M
-    public private(set) var state: M
+    public private(set) var machine: M
 
     public init(_ machine: M = M.initial) {
         self.machine = machine
-        self.state = machine
     }
 
     public func schedule(_ action: M.Action) {
         machine.apply(action)
-        state = machine
+        machine = machine
     }
 }
