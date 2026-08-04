2ef2b622dced2766399fa6e07a703660170c4e75
Alex Odawa <alex@squareup.com>
2026-08-04 12:13:08 +0200

    Restore SimpleError from deleted VerifiedMacro.swift

--- Sources/SwiftTLAPlugin/Plugin.swift
+++ Sources/SwiftTLAPlugin/Plugin.swift
@@ -1,6 +1,12 @@
 import SwiftCompilerPlugin
 import SwiftSyntaxMacros
 
+struct SimpleError: Error, CustomStringConvertible {
+    let message: String
+    init(_ msg: String) { self.message = msg }
+    var description: String { message }
+}
+
 @main
 struct SwiftTLAPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
