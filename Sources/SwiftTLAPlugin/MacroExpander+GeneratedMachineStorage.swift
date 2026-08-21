2d77441088fed9aca1a3dbccb44f311a230fb7a7
Alex Odawa <aodawa@mac.com>
2026-08-21 22:02:30 +0200

    Repair generated live collection projection

--- Sources/SwiftTLAPlugin/MacroExpander+GeneratedMachineStorage.swift
+++ Sources/SwiftTLAPlugin/MacroExpander+GeneratedMachineStorage.swift
@@ -22,7 +22,7 @@ extension MacroExpander {
                 guard !Self._identityRoutedActionOrdinals.contains(Self._actionOrdinal(for: action)) else {
                     throw GeneratedMachineError.identityRoutedActionRequiresID
                 }
-            """
+            return """
         let liveProjection = symmetricCollections.compactMap { collection in
             guard let ordinal = variables.firstIndex(where: { $0.formalName == collection.formalName }) else {
                 return nil
